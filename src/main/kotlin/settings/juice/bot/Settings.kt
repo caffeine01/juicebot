59f4d71d32382c7c5e3afff6a94c26f9911cba41
Juicyexe <juicyexe420@gmail.com>
2020-08-07 19:01:29 +0100

    add files

--- src/main/kotlin/settings/juice/bot/Settings.kt
+++ src/main/kotlin/settings/juice/bot/Settings.kt
@@ -1,5 +1,5 @@
 package settings.juice.bot
 
 object Settings {
-    const val BOT_TOKEN = ("NjYxNzg5NjI0OTM0NTk2NjE5.Xgwhng.GffhoXOBXqFv_GVrvJQal4pw9rg");
+    const val BOT_TOKEN = ("Your Token");
 }
